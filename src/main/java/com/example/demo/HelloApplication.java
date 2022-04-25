ab3c024d971f58b8b526ec6b0e564bae930483b1
beyzagumuse <e190503031@stud.tau.edu.tr>
2022-04-25 14:37:23 +0300

    Connect Database 01

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -15,6 +15,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        database db = new database();
+        db.connect();
     }
 
     public static void main(String[] args) {
