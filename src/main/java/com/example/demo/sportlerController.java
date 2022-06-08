0a0634a439e486cb6e33f7f7064b980fe5da5cf5
beyzagumuse <e190503031@stud.tau.edu.tr>
2022-06-08 10:18:49 +0300

    addSportler 01

--- src/main/java/com/example/demo/sportlerController.java
+++ src/main/java/com/example/demo/sportlerController.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.Classes.database;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class sportlerController {
 
@@ -23,12 +27,49 @@ public class sportlerController {
 
 
 
+
+    @FXML
+    private TextField addsportleradresse;
+
+    @FXML
+    private TextField addsportlerfr;
+
+    @FXML
+    private TextField addsportlerid;
+
+    @FXML
+    private TextField addsportlerkrank;
+
+    @FXML
+    private TextField addsportlermail;
+
+    @FXML
+    private TextField addsportlermv;
+
+    @FXML
+    private TextField addsportlername;
+
+    @FXML
+    private TextField addsportlertelno;
+
+
+
     @FXML
     void goaddSportler() {
         addSportlerpane.setVisible(true);
         deleteSportlerpane.setVisible(false);
         editsportlerpane.setVisible(false);
         detailspane.setVisible(false);
+    }
+
+    @FXML
+    private void addSportler() throws IOException {
+        database.addKurse(Integer.parseInt(addsportlerid.getText()), addsportlername.getText(),addsportlertelno.getText(),addsportleradresse.getText(),addsportlermail.getText(),Integer.parseInt(addsportlermv.getText()),Integer.parseInt(addsportlerfr.getText()),addsportlerkrank.getText());
+        database.printKurseData(Integer.parseInt(addSportler.getText()));
+
+        System.out.println("Person wurde zum Datenbank addiert.");
+        goKurspage();
+
     }
 
     @FXML
